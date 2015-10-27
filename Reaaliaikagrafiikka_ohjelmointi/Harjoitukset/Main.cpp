60b636b4c169dd80d6ee9d50947624195d673415
Nhaikou <nikokinnunen92@gmail.com>
2015-10-27 14:45:34 +0200

    Lisää laskuja

--- Reaaliaikagrafiikka_ohjelmointi/Harjoitukset/Main.cpp
+++ Reaaliaikagrafiikka_ohjelmointi/Harjoitukset/Main.cpp
@@ -42,6 +42,7 @@ public:
 		}
 	}
 
+	// Summa
 	myvector_t operator+(const myvector_t &b) const
 	{
 		if (GetSize() != b.GetSize())
@@ -57,15 +58,52 @@ public:
 
 		return v;
 	}
+	// Vähennys
+	myvector_t operator-(const myvector_t &b) const
+	{
+		if (GetSize() != b.GetSize())
+		{
+			std::cout << "ERROR" << std::endl;
+		}
+
+		myvector_t v(GetSize());
+		for (int i = 0; i < GetSize(); i++)
+		{
+			v.items[i] = items[i] - b.items[i];
+		}
+
+		return v;
+	}
+
+	// Kerto
+	myvector_t operator*(const myvector_t &b) const
+	{
+		if (GetSize() != b.GetSize())
+		{
+			std::cout << "ERROR" << std::endl;
+		}
+
+		myvector_t v(GetSize());
+		for (int i = 0; i < GetSize(); i++)
+		{
+			v.items[i] = items[i] * b.items[i];
+		}
+
+		return v;
+	}
 };
 
 
 int main()
 {
-	
+	// Vektori laskut
+	//----------------------------------------------------------------------
 	myvector_t<int> vec1(3);
 	myvector_t<int> vec2(3);
 	myvector_t<int> sumVec(0);
+	myvector_t<int> minVec(0);
+	myvector_t<int> multVec(0);
+
 
 	vec1.push(1);
 	vec1.push(2);
@@ -73,11 +111,23 @@ int main()
 
 	vec2.push(1);
 	vec2.push(1);
-	vec2.push(1);
+	vec2.push(2);
 
 	sumVec = vec1 + vec2;
-
+	minVec = vec1 - vec2;
+	multVec = vec1 * vec2;
+	std::cout << "Sum" << std::endl;
 	sumVec.Print();
+	std::cout << std::endl;
+	std::cout << "Min" << std::endl;
+	minVec.Print();
+	std::cout << std::endl;
+	std::cout << "Income" << std::endl;
+	multVec.Print();
+	std::cout << std::endl;
+	//---------------------------------------------------------------------
+
+
 
 	system("pause");
 	return 0;
